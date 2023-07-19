4dd79301aa7053d54d82ca8444796b2cede61640
Wayne <jwen087@uottawa.ca>
2023-07-19 13:07:57 -0400

    update

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,5 +1,4 @@
 import { Link } from 'react-router-dom';
-
 import logo from './logo.png';
 const Navbar = () => {
     return ( 
--- src/Test1.js
+++ src/Test1.js
@@ -2,20 +2,20 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
-function Test() {
+function ResponsiveAutoExample() {
   return (
     <Container>
       <Row>
-        <Col>1 of 2</Col>
-        <Col>2 of 2</Col>
+        <Col sm={8}>sm=8</Col>
+        <Col sm={4}>sm=4</Col>
       </Row>
       <Row>
-        <Col>1 of 3</Col>
-        <Col>2 of 3</Col>
-        <Col>3 of 3</Col>
+        <Col sm>sm=true</Col>
+        <Col sm>sm=true</Col>
+        <Col sm>sm=true</Col>
       </Row>
     </Container>
   );
 }
 
-export default Test;
+export default ResponsiveAutoExample;
